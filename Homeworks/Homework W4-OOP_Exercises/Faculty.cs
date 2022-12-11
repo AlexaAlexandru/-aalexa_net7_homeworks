44fab2a177bf9a440ad8c861155ed2238a44bb0b
AlexaAlexandru <117767211+AlexaAlexandru@users.noreply.github.com>
2022-12-11 16:52:09 +0200

    Week 4 Ex firstTry

---
+++ Homeworks/Homework W4-OOP_Exercises/Faculty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Homework_W4_OOP_Exercises
+{
+	public class Faculty
+	{
+        public string firstName;
+        public string lastName;
+        public int employeeId;
+		public List<string> subjectsTaught=new List<string>();
+        public Faculty(string firstname,string lastName,int employeeId,List<string> subjectsTaught)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.employeeId = employeeId;
+			this.subjectsTaught = subjectsTaught;
+
+		}
+		public string GetFullName()
+		{
+			string output = $"{this.firstName}{this.lastName}";
+			return output;
+		}
+
+		/*
+		public var GetSubjectsTaught()
+		{
+			subjectsTaught.Add(subjectsTaught);
+
+			return listSubject;
+			 
+		}
+		*/
+
+
+	}
+}
+
---
+++ Homeworks/Homework W4-OOP_Exercises/University.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Homework_W4_OOP_Exercises
+{
+	public class University
+	{
+		public string name;
+		public Student student;
+		public Faculty faculty;
+        List<string> listStudents = new List<string>();
+        public University(string name)
+		{
+			this.name = name;
+			this.student = student;
+			this.faculty = faculty;
+		}
+
+        public List<string> AddStudent(Student john)
+		{
+
+            listStudents.Add(student.GetFullName());
+
+			return listStudents;
+		}
+		/*
+        public void AddFaculty()
+		{
+			faculty.GetSubjectsTaught();
+		}
+		*/
+		public int GetStudentCount()
+		{ 
+			return listStudents.Count();
+		}
+	}
+}
+
